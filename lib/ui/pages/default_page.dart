16df1a983840641648d489227e2b09fa375c1c1b
Noh-Credit <synoh0918@gmail.com>
2026-03-20 12:43:49 +0900

    week3

---
+++ lib/ui/pages/default_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DefaultPage extends StatelessWidget {
+  const DefaultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('예제 화면'),
+      ),
+      body: Row(
+        children: [
+          Expanded(child: Container(color: Colors.red,)),
+          Expanded(child: Container(color: Colors.orange,)),
+          Expanded(child: Container(color: Colors.yellow,)),
+        ],
+      ),
+    );
+  }
+}
